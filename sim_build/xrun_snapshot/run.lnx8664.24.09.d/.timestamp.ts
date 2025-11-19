79ac336afae2cdbe92dcb66b159ed6551217f319
Vishvas Pancholi <pancholv@ter.teradyne.com>
2025-11-19 15:08:54 +0530

    Stop tracking .simvision and sim_build, add to gitignore

--- sim_build/xrun_snapshot/run.lnx8664.24.09.d/.timestamp.ts
+++
@@ -1,3 +0,0 @@
-1763445609 /u/pancholv/Desktop/sv_uvm/python_stuff/cocotb_projects/alu_v2/alu/rtl/alu_interface.sv
-1763445872 /u/pancholv/Desktop/sv_uvm/python_stuff/cocotb_projects/alu_v2/alu/rtl/top.sv
-1763444179 /u/pancholv/Desktop/sv_uvm/python_stuff/cocotb_projects/alu_v2/alu/rtl/alu.sv
